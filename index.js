8dbb6a027dda84700582a44ee13c7900593fa4e0
KshitijMagare19 <kshitij.magare19@vit.edu>
2022-12-26 02:23:58 +0530

    os

---
+++ index.js
@@ -0,0 +1,13 @@
+
+// common js module
+const obb = require("./second");
+console.log("hi",obb)
+
+const obj = require("os")
+console.log(obj.freemem())
+console.log(obj.homedir())
+console.log(obj.hostname())
+console.log(obj.platform())
+console.log(obj.release())
+console.log(obj.totalmem())
+console.log(obj.uptime())
---
+++ second.js
@@ -0,0 +1,6 @@
+object = {
+    Data : "private",
+    Id : 5555,
+    gr : 11911072
+}
+module.exports = object;

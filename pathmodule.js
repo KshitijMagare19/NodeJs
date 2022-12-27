dbac4aae766260e1409032bed7773c9420139a7e
KshitijMagare19 <kshitij.magare19@vit.edu>
2022-12-28 02:48:35 +0530

    path modules

---
+++ pathmodule.js
@@ -0,0 +1,10 @@
+const path = require('node:path');
+
+const a1 = path.basename('C:\\temp\\myfile.html');
+const a2 = path.dirname('C:\\temp\\myfile.html');
+
+console.log(a1)
+console.log(a2)
+
+const a3 =  path.extname('index.js');
+console.log(a3)
